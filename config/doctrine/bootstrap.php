5d1dd61fff66e45a024655c635e6de3cabc2729c
ArsTropica <aowilliams@arstropica.com>
2015-10-24 14:05:07 -0400

    Added Dependency Autoloading

--- config/doctrine/bootstrap.php
+++ config/doctrine/bootstrap.php
@@ -3,13 +3,20 @@
 // Include Composer Autoload (relative to project root).
 $dbParams = [];
 require_once __DIR__ . "/../../vendor/autoload.php";
-require_once __DIR__ . "/development.db.php";
+require_once __DIR__ . "/local.db.php";
 
 use Doctrine\ORM\Tools\Setup;
 use Doctrine\ORM\EntityManager;
 use Doctrine\Common\Annotations\AnnotationRegistry;
 
 $loader = require __DIR__ . '/../../vendor/autoload.php';
+Zend\Loader\AutoloaderFactory::factory ( array (
+		'Zend\Loader\StandardAutoloader' => array (
+				Zend\Loader\StandardAutoloader::LOAD_NS => array (
+						"Application" => __DIR__ . "/../../module/Application/src/Application"
+				)
+		)
+) );
 
 AnnotationRegistry::registerLoader(array(
 		$loader,
