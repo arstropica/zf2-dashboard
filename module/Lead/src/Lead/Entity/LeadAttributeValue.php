4038231d809648c615286757669bc3f671ed790e
ArsTropica <aowilliams@arstropica.com>
2015-11-06 22:51:53 -0500

    Added order property and getter/setter to access Attribute Order.

--- module/Lead/src/Lead/Entity/LeadAttributeValue.php
+++ module/Lead/src/Lead/Entity/LeadAttributeValue.php
@@ -1,134 +1,159 @@
 <?php
-
 namespace Lead\Entity;
-
 use Doctrine\ORM\Mapping as ORM;
 
 /**
  * LeadAttributeValue
  *
- * @ORM\Table(name="lead_attribute_values", indexes={@ORM\Index(name="idx_lead_id", columns={"lead_id"}), @ORM\Index(name="idx_attribute_id", columns={"attribute_id"})})
+ * @ORM\Table(name="lead_attribute_values",
+ * indexes={@ORM\Index(name="idx_lead_id", columns={"lead_id"}),
+ * @ORM\Index(name="idx_attribute_id", columns={"attribute_id"})})
  * @ORM\Entity
  */
 class LeadAttributeValue
 {
-    /**
-     * @var integer
-     *
-     * @ORM\Column(name="id", type="integer", nullable=false)
-     * @ORM\Id
-     * @ORM\GeneratedValue(strategy="IDENTITY")
-     */
-    private $id;
 
-    /**
-     * @var string
-     *
-     * @ORM\Column(name="value", type="text", length=65535, nullable=true)
-     */
-    private $value;
+	/**
+	 *
+	 * @var integer @ORM\Column(name="id", type="integer", nullable=false)
+	 *      @ORM\Id
+	 *      @ORM\GeneratedValue(strategy="IDENTITY")
+	 */
+	private $id;
 
-    /**
-     * @var \Lead\Entity\LeadAttribute
-     *
-     * @ORM\ManyToOne(targetEntity="Lead\Entity\LeadAttribute", inversedBy="values", cascade={"persist"})
-     * @ORM\JoinColumns({
-     *   @ORM\JoinColumn(name="attribute_id", referencedColumnName="id")
-     * })
-     */
-    private $attribute;
+	/**
+	 *
+	 * @var string @ORM\Column(name="value", type="text", length=65535,
+	 *      nullable=true)
+	 */
+	private $value;
 
-    /**
-     * @var \Lead\Entity\Lead
-     *
-     * @ORM\ManyToOne(targetEntity="Lead\Entity\Lead", inversedBy="attributes", cascade={"persist"})
-     * @ORM\JoinColumns({
-     *   @ORM\JoinColumn(name="lead_id", referencedColumnName="id")
-     * })
-     */
-    private $lead;
+	/**
+	 *
+	 * @var \Lead\Entity\LeadAttribute
+	 *      @ORM\ManyToOne(targetEntity="Lead\Entity\LeadAttribute",
+	 *      inversedBy="values", cascade={"persist"})
+	 *      @ORM\JoinColumns({
+	 *      @ORM\JoinColumn(name="attribute_id", referencedColumnName="id")
+	 *      })
+	 */
+	private $attribute;
 
+	private $order;
 
+	/**
+	 *
+	 * @var \Lead\Entity\Lead @ORM\ManyToOne(targetEntity="Lead\Entity\Lead",
+	 *      inversedBy="attributes", cascade={"persist"})
+	 *      @ORM\JoinColumns({
+	 *      @ORM\JoinColumn(name="lead_id", referencedColumnName="id")
+	 *      })
+	 */
+	private $lead;
 
-    /**
-     * Get id
-     *
-     * @return integer
-     */
-    public function getId()
-    {
-        return $this->id;
-    }
+	/**
+	 * Get id
+	 *
+	 * @return integer
+	 */
+	public function getId ()
+	{
+		return $this->id;
+	}
 
-    /**
-     * Set value
-     *
-     * @param string $value
-     *
-     * @return LeadAttributeValue
-     */
-    public function setValue($value)
-    {
-        $this->value = $value;
+	/**
+	 * Set value
+	 *
+	 * @param string $value        	
+	 *
+	 * @return LeadAttributeValue
+	 */
+	public function setValue ($value)
+	{
+		$this->value = $value;
+		
+		return $this;
+	}
 
-        return $this;
-    }
+	/**
+	 * Get value
+	 *
+	 * @return string
+	 */
+	public function getValue ()
+	{
+		return $this->value;
+	}
 
-    /**
-     * Get value
-     *
-     * @return string
-     */
-    public function getValue()
-    {
-        return $this->value;
-    }
+	/**
+	 * Set attribute
+	 *
+	 * @param \Lead\Entity\LeadAttribute $attribute        	
+	 *
+	 * @return LeadAttributeValue
+	 */
+	public function setAttribute (\Lead\Entity\LeadAttribute $attribute = null)
+	{
+		$this->attribute = $attribute;
+		
+		return $this;
+	}
 
-    /**
-     * Set attribute
-     *
-     * @param \Lead\Entity\LeadAttribute $attribute
-     *
-     * @return LeadAttributeValue
-     */
-    public function setAttribute(\Lead\Entity\LeadAttribute $attribute = null)
-    {
-        $this->attribute = $attribute;
+	/**
+	 * Get attribute
+	 *
+	 * @return \Lead\Entity\LeadAttribute
+	 */
+	public function getAttribute ()
+	{
+		return $this->attribute;
+	}
 
-        return $this;
-    }
+	/**
+	 * Get Attribute Order
+	 *
+	 * @return integer
+	 */
+	public function getOrder ()
+	{
+		return $this->attribute->getAttributeOrder();
+	}
 
-    /**
-     * Get attribute
-     *
-     * @return \Lead\Entity\LeadAttribute
-     */
-    public function getAttribute()
-    {
-        return $this->attribute;
-    }
+	/**
+	 * Set Attribute Order
+	 *
+	 * @param integer $order        	
+	 *
+	 * @return LeadAttributeValue
+	 */
+	public function setOrder ($order)
+	{
+		$this->attribute->setAttributeOrder($order);
+		
+		return $this;
+	}
 
-    /**
-     * Set lead
-     *
-     * @param \Lead\Entity\Lead $lead
-     *
-     * @return LeadAttributeValue
-     */
-    public function setLead(\Lead\Entity\Lead $lead = null)
-    {
-        $this->lead = $lead;
+	/**
+	 * Set lead
+	 *
+	 * @param \Lead\Entity\Lead $lead        	
+	 *
+	 * @return LeadAttributeValue
+	 */
+	public function setLead (\Lead\Entity\Lead $lead = null)
+	{
+		$this->lead = $lead;
+		
+		return $this;
+	}
 
-        return $this;
-    }
-
-    /**
-     * Get lead
-     *
-     * @return \Lead\Entity\Lead
-     */
-    public function getLead()
-    {
-        return $this->lead;
-    }
+	/**
+	 * Get lead
+	 *
+	 * @return \Lead\Entity\Lead
+	 */
+	public function getLead ()
+	{
+		return $this->lead;
+	}
 }
