dfcf9fc0362bbe4766993fe59b63d47b77ed87f1
ArsTropica <aowilliams@arstropica.com>
2015-11-06 23:11:43 -0500

    Added Google API Authorization to authorize method.

--- module/TenStreet/src/TenStreet/Controller/Factory/SoapClientControllerFactory.php
+++ module/TenStreet/src/TenStreet/Controller/Factory/SoapClientControllerFactory.php
@@ -3,7 +3,6 @@ namespace TenStreet\Controller\Factory;
 use Zend\ServiceManager\FactoryInterface;
 use Zend\ServiceManager\ServiceLocatorInterface;
 use TenStreet\Controller\SoapClientController;
-use OAuth2\Server as OAuth2Server;
 
 class SoapClientControllerFactory implements FactoryInterface
 {
@@ -11,12 +10,6 @@ class SoapClientControllerFactory implements FactoryInterface
 	public function createService (ServiceLocatorInterface $service)
 	{
 		$parentService = $service->getServiceLocator();
-		$oauth2Server = $parentService->get('OAuth2Server');
-		
-		if ($oauth2Server instanceof OAuth2Server) {
-			return new SoapClientController($oauth2Server, $parentService);
-		}
-		
-		return false;
+		return new SoapClientController($parentService);
 	}
 }
