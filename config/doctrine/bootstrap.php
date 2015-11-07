3b4b7e0d195276754a99e792937927b30ca2a082
ArsTropica <aowilliams@arstropica.com>
2015-11-07 13:29:12 -0500

    Conditionally load db configuration according to dev or prod environment.

--- config/doctrine/bootstrap.php
+++ config/doctrine/bootstrap.php
@@ -2,21 +2,32 @@
 // bootstrap.php
 // Include Composer Autoload (relative to project root).
 $dbParams = [];
+/**
+ * The dev home dir name
+ *
+ * @var string
+ */
+$devdir = 'zf2';
+$cwd = getcwd();
+$isDevMode = strstr($cwd, "/{$devdir}/");
+
 require_once __DIR__ . "/../../vendor/autoload.php";
-require_once __DIR__ . "/local.db.php";
+require_once __DIR__ . "/" . ($isDevMode ? "development" : "local") . ".db.php";
 
 use Doctrine\ORM\Tools\Setup;
 use Doctrine\ORM\EntityManager;
 use Doctrine\Common\Annotations\AnnotationRegistry;
 
 $loader = require __DIR__ . '/../../vendor/autoload.php';
-Zend\Loader\AutoloaderFactory::factory ( array (
-		'Zend\Loader\StandardAutoloader' => array (
-				Zend\Loader\StandardAutoloader::LOAD_NS => array (
-						"Application" => __DIR__ . "/../../module/Application/src/Application"
+Zend\Loader\AutoloaderFactory::factory(
+		array(
+				'Zend\Loader\StandardAutoloader' => array(
+						Zend\Loader\StandardAutoloader::LOAD_NS => array(
+								"Application" => __DIR__ .
+										 "/../../module/Application/src/Application"
+						)
 				)
-		)
-) );
+		));
 
 AnnotationRegistry::registerLoader(array(
 		$loader,
@@ -30,15 +41,6 @@ $paths = [
 		realpath('module/Event/src/Event/Entity'),
 		realpath('module/Api/src/Api/Entity')
 ];
-
-/**
- * The dev home dir name
- * 
- * @var string
- */
-$devdir = 'zf2';
-$cwd = getcwd();
-$isDevMode = strstr($cwd, "/{$devdir}/");
 
 $config = Setup::createAnnotationMetadataConfiguration($paths, $isDevMode, null, 
 		null, false);
