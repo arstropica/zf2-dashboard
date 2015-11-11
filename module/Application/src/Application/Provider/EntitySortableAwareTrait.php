7ff2847aa4899aeb09a743785e0a801fe5f55b6e
ArsTropica <aowilliams@arstropica.com>
2015-11-11 16:19:39 -0500

    Astracted fetch and reindex functionality into separate methods.

--- module/Application/src/Application/Provider/EntitySortableAwareTrait.php
+++ module/Application/src/Application/Provider/EntitySortableAwareTrait.php
@@ -10,7 +10,7 @@ use Doctrine\ORM\EntityManager;
 trait EntitySortableAwareTrait
 {
 
-	function fetchByOrder ($sort, $order = 'asc', $reindex = true)
+	function fetchByOrder ($sort, $order = 'asc', $reindex = true, $fixed_id = null)
 	{
 		/* @var $qb \Doctrine\ORM\QueryBuilder */
 		$qb = $this->getEntityManager()->createQueryBuilder();
@@ -22,14 +22,8 @@ trait EntitySortableAwareTrait
 		$results = $query->getArrayResult();
 		
 		if ($reindex) {
-			$reindexed_results = [];
-			if ($results) {
-				foreach ($results as $index => $result) {
-					$result[$sort] = $index;
-					$reindexed_results[$index] = $result;
-				}
-			}
-			$results = $reindexed_results;
+			$reindexed_results = $this->_reindex($results, $sort, $fixed_id);
+			$results = $this->_reindex($reindexed_results, $sort);
 		}
 		return $results;
 	}
@@ -46,6 +40,39 @@ trait EntitySortableAwareTrait
 		}
 		$out = array_splice($array, $from, 1);
 		array_splice($array, $to, 0, $out);
+	}
+
+	protected function _reindex ($array, $sort, $fixed_id = null)
+	{
+		$reindexed = [];
+		$new_index = 0;
+		$fixed_index = null;
+		if ($array) {
+			for ($i = 0; $i < count($array); $i ++) {
+				$result = $array[$i];
+				if (isset($fixed_id) && $fixed_id == $result['id']) {
+					$fixed_index = $result[$sort];
+					if ($fixed_index === $new_index) {
+						$reindexed[$new_index] = $result;
+					} elseif (isset($reindexed[$fixed_index])) {
+						$reindexed[$new_index] = $reindexed[$fixed_index];
+						$reindexed[$fixed_index] = $result;
+					} else {
+						$reindexed[$fixed_index] = $result;
+					}
+				} elseif (isset($reindexed[$new_index])) {
+					$new_index ++;
+					$result[$sort] = $new_index;
+					$reindexed[$new_index] = $result;
+				} else {
+					$result[$sort] = $new_index;
+					$reindexed[$new_index] = $result;
+				}
+				$new_index ++;
+			}
+			$array = $reindexed;
+		}
+		return $array;
 	}
 
 	public abstract function getEntityClass ();
