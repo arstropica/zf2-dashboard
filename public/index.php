7c53c9a947898e62ba07552e2299f7a02ba98738
ArsTropica <aowilliams@arstropica.com>
2015-11-11 16:31:25 -0500

    No change...

--- public/index.php
+++ public/index.php
@@ -4,21 +4,20 @@
  * to the application root now.
  */
 define('REQUEST_MICROTIME', microtime(true));
-if (isset($_SERVER['APPLICATION_ENV']) &&
-		 $_SERVER['APPLICATION_ENV'] == 'development') {
-	error_reporting(E_ALL);
-	ini_set("display_errors", 1);
+if (isset($_SERVER['APPLICATION_ENV']) && $_SERVER['APPLICATION_ENV'] == 'development') {
+    error_reporting(E_ALL);
+    ini_set("display_errors", 1);
 }
 
 chdir(dirname(__DIR__));
 
 // Decline static file requests back to the PHP built-in webserver
 if (php_sapi_name() === 'cli-server') {
-	$path = realpath(__DIR__ . parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
-	if (__FILE__ !== $path && is_file($path)) {
-		return false;
-	}
-	unset($path);
+    $path = realpath(__DIR__ . parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
+    if (__FILE__ !== $path && is_file($path)) {
+        return false;
+    }
+    unset($path);
 }
 
 // Setup autoloading
