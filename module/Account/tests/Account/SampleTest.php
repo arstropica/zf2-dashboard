6de4a378da46a093bcb307149905a03e3affc788
ArsTropica <aowilliams@arstropica.com>
2015-11-23 12:38:43 -0500

    Started Unit Testing with Mock Doctrine Entities.

--- module/Account/tests/Account/SampleTest.php
+++ module/Account/tests/Account/SampleTest.php
@@ -2,7 +2,7 @@
 /**
  * Zend Framework (http://framework.zend.com/)
  *
- * @link      http://github.com/zendframework/Account for the canonical source repository
+ * @link      http://github.com/zendframework/Email for the canonical source repository
  * @copyright Copyright (c) 2005-2014 Zend Technologies USA Inc. (http://www.zend.com)
  * @license   http://framework.zend.com/license/new-bsd New BSD License
  */
