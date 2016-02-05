c7504c70ea775f03d006e2735b15206d3ddc784a
ArsTropica <aowilliams@arstropica.com>
2016-02-05 18:33:30 -0500

    Added lead export functionality

--- module/Lead/src/Lead/Controller/TenStreetController.php
+++ module/Lead/src/Lead/Controller/TenStreetController.php
@@ -1,10 +1,12 @@
 <?php
+
 /**
  *
  * @author arstropica
  *        
  */
 namespace Lead\Controller;
+
 use Application\Controller\AbstractCrudController;
 use Zend\Paginator\Paginator;
 use Doctrine\ORM\QueryBuilder as Builder;
@@ -14,38 +16,41 @@ use DoctrineORMModule\Stdlib\Hydrator\DoctrineEntity as DoctrineHydrator;
 use Zend\Stdlib\ResponseInterface as Response;
 use Application\Hydrator\Strategy\DateTimeStrategy;
 use Lead\Form\SubmitForm;
+use Lead\Entity\Lead;
 
-class TenStreetController extends AbstractCrudController
-{
-
+class TenStreetController extends AbstractCrudController {
+	
 	protected $defaultSort = 'id';
-
+	
 	protected $defaultOrder = 'desc';
-
+	
 	protected $defaultPageSize = 10;
-
+	
 	protected $paginatorRange = 5;
-
+	
 	protected $uniqueField = null;
-
+	
 	protected $uniqueEntityMessage = null;
-
+	
 	protected $successSubmitMessage = 'The Leads(s) were successfully submitted.';
-
+	
 	protected $errorEditMessage = 'There was a problem editing your Leads(s).';
-
+	
 	protected $errorSubmitMessage = 'There was a problem submitting your Lead(s).';
 
-	public function listAction ()
+	public function listAction()
 	{
 		$pagerAction = $this->handlePager();
 		$limit = $this->getLimit($this->defaultPageSize);
 		// $limit = $this->getRequest()->getQuery('limit',
 		// $this->defaultPageSize);
 		
-		$page = $this->getRequest()->getQuery('page', 0);
-		$sort = $this->getRequest()->getQuery('sort', $this->defaultSort);
-		$order = $this->getRequest()->getQuery('order', $this->defaultOrder);
+		$page = $this->getRequest()
+			->getQuery('page', 0);
+		$sort = $this->getRequest()
+			->getQuery('sort', $this->defaultSort);
+		$order = $this->getRequest()
+			->getQuery('order', $this->defaultOrder);
 		
 		if (empty($sort)) {
 			$sort = $this->defaultSort;
@@ -57,7 +62,8 @@ class TenStreetController extends AbstractCrudController
 		}
 		
 		/* @var $qb \Doctrine\ORM\QueryBuilder */
-		$qb = $this->getEntityManager()->createQueryBuilder();
+		$qb = $this->getEntityManager()
+			->createQueryBuilder();
 		$qb->add('select', 'e')
 			->add('from', $this->getEntityClass() . ' e')
 			->innerJoin('e.account', 'account')
@@ -72,47 +78,47 @@ class TenStreetController extends AbstractCrudController
 		
 		$pager = $this->getPagerForm($limit);
 		
-		$paginator = new Paginator(
-				new DoctrinePaginator(new LosPaginator($qb, false)));
+		$paginator = new Paginator(new DoctrinePaginator(new LosPaginator($qb, false)));
 		$paginator->setDefaultItemCountPerPage($limit);
 		$paginator->setCurrentPageNumber($page);
 		$paginator->setPageRange($this->paginatorRange);
 		
-		$ui = [
-				'table' => [
-						"description" => [
+		$ui = [ 
+				'table' => [ 
+						"description" => [ 
 								"col" => 3,
 								"label" => "Name",
-								"sort" => false
+								"sort" => false 
 						],
-						"event" => [
+						"event" => [ 
 								"col" => 3,
 								"label" => "Latest TenStreet Event",
-								"sort" => false
+								"sort" => false 
 						],
-						"account" => [
+						"account" => [ 
 								"col" => 2,
 								"label" => "Account",
-								"sort" => false
+								"sort" => false 
 						],
-						"timecreated" => [
+						"timecreated" => [ 
 								"col" => 2,
 								"label" => "Date",
-								"sort" => true
-						]
-				]
+								"sort" => true 
+						] 
+				] 
 		];
 		
 		$filters = $this->getFilterForm($this->params()
 			->fromQuery());
-		$post = $this->params()->fromPost();
+		$post = $this->params()
+			->fromPost();
 		
-		$redirectUrl = $this->url()->fromRoute('services/tenstreet', 
-				[
-						'action' => 'list'
-				], true);
+		$redirectUrl = $this->url()
+			->fromRoute('services/tenstreet', [ 
+				'action' => 'list' 
+		], true);
 		
-		if (! $pagerAction) {
+		if (!$pagerAction) {
 			$prg = $this->prg($redirectUrl, true);
 		} else {
 			$prg = false;
@@ -122,48 +128,49 @@ class TenStreetController extends AbstractCrudController
 			return $prg;
 		} elseif ($prg === false) {
 			$form = $this->getListForm($paginator);
-			return [
+			return [ 
 					'paginator' => $paginator,
 					'sort' => $sort,
 					'order' => $order,
 					'page' => $page,
 					'pager' => $pager,
-					'query' => $this->params()->fromQuery(),
+					'query' => $this->params()
+						->fromQuery(),
 					'form' => $form,
 					'filters' => $filters,
-					'ui' => $ui
+					'ui' => $ui 
 			];
 		}
 		
 		$form = $this->getListForm($paginator, $prg);
 		
-		if ($prg && isset($prg['sel'])) {
+		if ($prg && isset($prg ['sel'])) {
 			$res = true;
-			foreach ($prg['sel'] as $lead_id => $one) {
+			foreach ( $prg ['sel'] as $lead_id => $one ) {
 				if ($one) {
 					$response = $this->submit($lead_id);
 					$res = $response ? $res : false;
 				}
 			}
 			if ($res) {
-				$this->flashMessenger()->addSuccessMessage(
-						$this->getServiceLocator()
-							->get('translator')
-							->translate($this->successSubmitMessage));
+				$this->flashMessenger()
+					->addSuccessMessage($this->getServiceLocator()
+					->get('translator')
+					->translate($this->successSubmitMessage));
 			} else {
-				$this->flashMessenger()->addErrorMessage(
-						$this->getServiceLocator()
-							->get('translator')
-							->translate($this->errorSubmitMessage));
+				$this->flashMessenger()
+					->addErrorMessage($this->getServiceLocator()
+					->get('translator')
+					->translate($this->errorSubmitMessage));
 			}
 		}
-		return $this->redirect()->toRoute('services/tenstreet', 
-				[
-						'action' => 'list'
-				], true);
+		return $this->redirect()
+			->toRoute('services/tenstreet', [ 
+				'action' => 'list' 
+		], true);
 	}
 
-	public function viewAction ()
+	public function viewAction()
 	{
 		$id = $this->getEvent()
 			->getRouteMatch()
@@ -173,22 +180,22 @@ class TenStreetController extends AbstractCrudController
 		$objRepository = $em->getRepository($this->getEntityClass());
 		$entity = $objRepository->find($id);
 		
-		return [
-				'entity' => $entity
+		return [ 
+				'entity' => $entity 
 		];
 	}
 
-	public function submitAction ()
+	public function submitAction()
 	{
 		$id = $this->getEvent()
 			->getRouteMatch()
 			->getParam('id', 0);
 		
-		if (! $id) {
-			return $this->redirect()->toRoute('services/tenstreet', 
-					[
-							'action' => 'list'
-					], true);
+		if (!$id) {
+			return $this->redirect()
+				->toRoute('services/tenstreet', [ 
+					'action' => 'list' 
+			], true);
 		}
 		
 		$em = $this->getEntityManager();
@@ -198,34 +205,106 @@ class TenStreetController extends AbstractCrudController
 		$response = $this->submit($id);
 		
 		if ($response) {
-			$this->flashMessenger()->addSuccessMessage(
-					$this->getServiceLocator()
-						->get('translator')
-						->translate($this->successSubmitMessage));
+			$this->flashMessenger()
+				->addSuccessMessage($this->getServiceLocator()
+				->get('translator')
+				->translate($this->successSubmitMessage));
 		} else {
-			$this->flashMessenger()->addErrorMessage(
-					$this->getServiceLocator()
-						->get('translator')
-						->translate($this->errorSubmitMessage));
+			$this->flashMessenger()
+				->addErrorMessage($this->getServiceLocator()
+				->get('translator')
+				->translate($this->errorSubmitMessage));
 		}
 		
-		return [
-				'entity' => $entity
+		return [ 
+				'entity' => $entity 
 		];
 	}
 
-	public function getForm ($entityClass = null)
+	public function exportAction()
+	{
+		$results = array ();
+		$labels = array ();
+		$headings = array ();
+		
+		$sort = $this->getRequest()
+			->getQuery('sort', $this->defaultSort);
+		$order = $this->getRequest()
+			->getQuery('order', $this->defaultOrder);
+		
+		if (empty($sort)) {
+			$sort = $this->defaultSort;
+		}
+		
+		/* @var $qb \Doctrine\ORM\QueryBuilder */
+		$qb = $this->getEntityManager()
+			->createQueryBuilder();
+		$qb->add('select', 'e')
+			->add('from', $this->getEntityClass() . ' e')
+			->innerJoin('e.account', 'account')
+			->leftJoin('account.apis', 'apis')
+			->where('apis.name = :tenstreet')
+			->orderBy('e.' . $sort, $order)
+			->setParameter('tenstreet', 'Tenstreet');
+		$qb = $this->handleSearch($qb);
+		
+		$entityManager = $this->getEntityManager();
+		$hydrator = new DoctrineHydrator($entityManager);
+		$leadPrototype = new Lead();
+		$leadArray = $hydrator->extract($leadPrototype);
+		
+		$results = $qb->getQuery()
+			->getResult();
+		
+		$em = $this->getEntityManager();
+		$attributeRepository = $em->getRepository("Lead\\Entity\\LeadAttribute");
+		
+		$attributes = $attributeRepository->getUniqueArray();
+		
+		$headings = [ 
+				'lead' => [ 
+						'account' => [ 
+								'name' => 'Account' 
+						],
+						'timecreated' => 'Time Created',
+						'referrer' => 'Referrer',
+						'ipaddress' => 'IP Address',
+						'attributes' => $attributes 
+				] 
+		];
+		
+		foreach ( $headings ['lead'] as $property => $field ) {
+			$key = $property;
+			if (is_array($field)) {
+				foreach ( $field as $externalKey => $value ) {
+					$key = $property . "[{$externalKey}]";
+					$labels [$key] = $value;
+				}
+			} else {
+				$labels [$key] = $field;
+			}
+		}
+		
+		$this->exportHeadings = array_values($labels);
+		
+		return $this->csvExport('TenStreet Lead Report (' . date('Y-m-d') . ').csv', $this->exportHeadings, $results, array (
+				$this,
+				'extractLead' 
+		));
+	}
+
+	public function getForm($entityClass = null)
 	{
 		$form = parent::getForm($entityClass);
 		
 		if ($form) {
 			$entityClass = $entityClass ?  : $this->getEntityClass();
-			$hydrator = new DoctrineHydrator($this->getEntityManager(), 
-					$entityClass);
+			$hydrator = new DoctrineHydrator($this->getEntityManager(), $entityClass);
 			$hydrator->addStrategy('timecreated', new DateTimeStrategy());
 			$form->setHydrator($hydrator);
 			if ($form->has('submit')) {
-				$form->get('submit')->setLabel('Save');
+				$form->get('submit')
+					->setLabel('Save');
 			}
 			if ($form->has('cancelar')) {
 				$form->get('cancelar')
@@ -236,77 +315,74 @@ class TenStreetController extends AbstractCrudController
 		return $form;
 	}
 
-	protected function getListForm (Paginator $paginator, $data = [])
+	protected function getListForm(Paginator $paginator, $data = [])
 	{
 		$form = new SubmitForm('leadtenstreetform');
 		
 		if ($paginator->count() > 0) {
 			// Batch Form
 			
-			foreach ($paginator as $entity) {
-				$cbx = new \Zend\Form\Element\Checkbox(
-						"sel[" . $entity->getId() . "]");
+			foreach ( $paginator as $entity ) {
+				$cbx = new \Zend\Form\Element\Checkbox("sel[" . $entity->getId() . "]");
 				$form->add($cbx);
 			}
 		}
 		if ($data) {
 			$form->setData($data);
-			if (! $form->isValid()) {
-				$form->setData(array());
+			if (!$form->isValid()) {
+				$form->setData(array ());
 			}
 		}
 		return $form;
 	}
 
-	protected function getFilterForm ($data = array())
+	protected function getFilterForm($data = array())
 	{
 		$sl = $this->getServiceLocator();
-		$form = $sl->get('FormElementManager')->get('Lead\Form\FilterForm');
+		$form = $sl->get('FormElementManager')
+			->get('Lead\Form\FilterForm');
 		$form->remove('referrer');
 		$form->setInputFilter($form->getInputFilter());
 		if ($data) {
 			$form->setData($data);
-			if (! $form->isValid()) {
-				$form->setData(array());
+			if (!$form->isValid()) {
+				$form->setData(array ());
 			}
 		}
 		return $form;
 	}
 
-	public function handleSearch (Builder $qb)
+	public function handleSearch(Builder $qb)
 	{
-		$query = $this->getRequest()->getQuery();
-		$filters = [
+		$query = $this->getRequest()
+			->getQuery();
+		$filters = [ 
 				'daterange',
-				'account'
+				'account' 
 		];
 		if ($query) {
-			$where = [];
-			$params = [];
-			foreach ($filters as $condition) {
-				if (isset($query[$condition]) && "" !== $query[$condition]) {
+			$where = [ ];
+			$params = [ ];
+			foreach ( $filters as $condition ) {
+				if (isset($query [$condition]) && "" !== $query [$condition]) {
 					switch ($condition) {
-						case 'daterange':
-							list ($from, $to) = array_map(
-									function  ($d)
-									{
-										return date('Y-m-d', strtotime($d));
-									}, explode("-", $query[$condition]));
-							$where['from'] = $from . ' 00:00:00';
-							$where['to'] = $to . ' 23:59:59';
-							$qb->andWhere(
-									$qb->expr()
-										->between("e.timecreated", ":from", 
-											":to"));
+						case 'daterange' :
+							list ( $from, $to ) = array_map(function ($d) {
+								return date('Y-m-d', strtotime($d));
+							}, explode("-", $query [$condition]));
+							$where ['from'] = $from . ' 00:00:00';
+							$where ['to'] = $to . ' 23:59:59';
+							$qb->andWhere($qb->expr()
+								->between("e.timecreated", ":from", ":to"));
 							break;
-						case 'account':
-							switch ($query[$condition]) {
-								case 'none':
+						case 'account' :
+							switch ($query [$condition]) {
+								case 'none' :
 									$qb->leftJoin('e.account', 'a');
 									$qb->andWhere("a.id IS NULL");
 									break;
-								default:
-									$where['id'] = $query[$condition];
+								default :
+									$where ['id'] = $query [$condition];
 									$qb->leftJoin('e.account', 'a');
 									$qb->andWhere("a.id = :id");
 									break;
@@ -316,7 +392,7 @@ class TenStreetController extends AbstractCrudController
 				}
 			}
 			if ($where) {
-				foreach ($where as $key => $value) {
+				foreach ( $where as $key => $value ) {
 					$qb->setParameter($key, $value);
 				}
 			}
@@ -324,13 +400,70 @@ class TenStreetController extends AbstractCrudController
 		return $qb;
 	}
 
-	protected function submit ($id)
+	protected function submit($id)
 	{
 		// Do something with API Key ???
 		$result = $this->getServiceLocator()
 			->get('TenStreet\Service\PostClientData')
 			->send($id);
 		
-		return isset($result['error']) ? false : true;
+		return isset($result ['error']) ? false : true;
 	}
+
+	public function extractLead(Lead $lead)
+	{
+		$headings = $this->exportHeadings;
+		
+		$entityManager = $this->getEntityManager();
+		$hydrator = new DoctrineHydrator($entityManager);
+		$leadArray = $hydrator->extract($lead);
+		$output = array_combine($headings, array_pad([ ], count($headings), ""));
+		
+		foreach ( $headings as $heading ) {
+			switch ($heading) {
+				case "Account" :
+					$account = $lead->getAccount();
+					$output [$heading] = $account ? $account->getName() : "N/A";
+					break;
+				case "Time Created" :
+					$time = $lead->getTimecreated();
+					if ($time instanceof \DateTime) {
+						$time = date_format($time, 'Y-m-d H:i:s');
+					}
+					$output [$heading] = $time;
+					break;
+				case "Referrer" :
+					$output [$heading] = $lead->getReferrer();
+					break;
+				case "IP Address" :
+					$output [$heading] = $lead->getIpaddress();
+					break;
+				default :
+					$attribute = $lead->findAttribute($heading);
+					if (!$attribute) {
+						$attributes = $lead->getAttributes(true)
+							->filter(function ($attribute) use($heading) {
+							$real_attribute = false;
+							$attribute_desc = false;
+							if ($attribute) {
+								$real_attribute = $attribute->getAttribute();
+							}
+							if ($real_attribute) {
+								$attribute_desc = $real_attribute->getAttributeDesc();
+							}
+							return $attribute_desc == $heading;
+						});
+						if ($attributes->count() > 0) {
+							$attribute = $attributes->first();
+						}
+					}
+					if ($attribute) {
+						$output [$heading] = $attribute->getValue();
+					}
+					break;
+			}
+		}
+		return $output;
+	}
+
 }
