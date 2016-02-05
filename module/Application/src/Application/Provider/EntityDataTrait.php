1966f0bd374a5c5fead0dd6f26183303d4b52ff1
ArsTropica <aowilliams@arstropica.com>
2016-02-05 18:18:14 -0500

    Removed service objects from entity / array conversion. Formatted DateTime object for Serialization. Refined magic method identification to handle underscore properties.

--- module/Application/src/Application/Provider/EntityDataTrait.php
+++ module/Application/src/Application/Provider/EntityDataTrait.php
@@ -1,42 +1,62 @@
 <?php
+
 namespace Application\Provider;
+
 use Doctrine\ORM\Proxy\Proxy;
+use Application\Service\ElasticSearch\SearchableEntityInterface;
+use Zend\ServiceManager\ServiceLocatorInterface;
 
 /**
  *
  * @author arstropica
  *        
  */
-trait EntityDataTrait
-{
+trait EntityDataTrait {
 
 	/**
 	 *
 	 * @return array
 	 */
-	public function toArray ()
+	public function toArray()
 	{
 		$data = get_object_vars($this);
 		
-		$data = array_filter($data, 
-				function  ($element)
-				{
-					return ! is_object($element);
-				});
+		$data = array_filter($data, function ($element) {
+			return !is_object($element);
+		});
 		
 		if ($this instanceof Proxy) {
 			$originClassName = get_parent_class($this);
 			
-			foreach ($data as $key => $value) {
-				if (! property_exists($originClassName, $key)) {
-					unset($data[$key]);
+			foreach ( $data as $key => $value ) {
+				if (!property_exists($originClassName, $key)) {
+					unset($data [$key]);
 				}
 			}
 		}
 		
-		foreach ($data as $key => $value) {
-			if (method_exists($this, 'get' . ucfirst($key))) {
-				$data[$key] = $this->{'get' . ucfirst($key)}();
+		if ($this instanceof SearchableEntityInterface) {
+			unset($data ['searchManager']);
+		}
+		
+		if ($this instanceof ServiceLocatorInterface) {
+			unset($data ['serviceLocator']);
+		}
+		
+		foreach ( $data as $key => $value ) {
+			$slug = ucfirst(preg_replace('/^[^\w]/i', '', $key));
+			if (method_exists($this, 'get' . $slug)) {
+				try {
+					$value = $this->{'get' . $slug}();
+					if ($value instanceof \DateTime) {
+						$value = $value->format('c');
+					}
+					if (!is_object($value)) {
+						$data [$key] = $value;
+					}
+				} catch ( \Exception $e ) {
+					continue;
+				}
 			}
 		}
 		
