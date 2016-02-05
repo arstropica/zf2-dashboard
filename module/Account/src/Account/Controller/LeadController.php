b61781b3f7cb1041855dc2b4f673921d58af9bb5
ArsTropica <aowilliams@arstropica.com>
2016-02-05 17:54:36 -0500

    added Last Submitted field

--- module/Account/src/Account/Controller/LeadController.php
+++ module/Account/src/Account/Controller/LeadController.php
@@ -1,5 +1,7 @@
 <?php
+
 namespace Account\Controller;
+
 use LosBase\Controller\AbstractCrudController;
 use Zend\Paginator\Paginator;
 use Doctrine\ORM\QueryBuilder as Builder;
@@ -13,37 +15,40 @@ use Application\Hydrator\Strategy\DateTimeStrategy;
  * @author arstropica
  *        
  */
-class LeadController extends AbstractCrudController
-{
-
-	protected $defaultSort = 'id';
-
+class LeadController extends AbstractCrudController {
+	
+	protected $defaultSort = 'lastsubmitted';
+	
 	protected $defaultOrder = 'desc';
-
+	
 	protected $defaultPageSize = 10;
-
+	
 	protected $paginatorRange = 5;
-
+	
 	protected $uniqueField = null;
-
+	
 	protected $uniqueEntityMessage = null;
-
+	
 	protected $successAddMessage = 'The Lead was successfully added.';
-
+	
 	protected $successEditMessage = 'The Lead(s) were successfully assigned.';
-
+	
 	protected $successDeleteMessage = 'The Lead was successfully deleted.';
-
+	
 	protected $errorEditMessage = 'There was a problem assigning your Lead(s).';
-
+	
 	protected $errorDeleteMessage = 'There was a problem deleting your Lead.';
 
-	public function listAction ()
+	public function listAction()
 	{
-		$page = $this->getRequest()->getQuery('page', 0);
-		$limit = $this->getRequest()->getQuery('limit', $this->defaultPageSize);
-		$sort = $this->getRequest()->getQuery('sort', $this->defaultSort);
-		$order = $this->getRequest()->getQuery('order', $this->defaultOrder);
+		$page = $this->getRequest()
+			->getQuery('page', 0);
+		$limit = $this->getRequest()
+			->getQuery('limit', $this->defaultPageSize);
+		$sort = $this->getRequest()
+			->getQuery('sort', $this->defaultSort);
+		$order = $this->getRequest()
+			->getQuery('order', $this->defaultOrder);
 		
 		$id = $this->getEvent()
 			->getRouteMatch()
@@ -59,7 +64,8 @@ class LeadController extends AbstractCrudController
 		}
 		
 		/* @var $qb \Doctrine\ORM\QueryBuilder */
-		$qb = $this->getEntityManager()->createQueryBuilder();
+		$qb = $this->getEntityManager()
+			->createQueryBuilder();
 		$qb->add('select', 'e')
 			->add('from', '\Lead\Entity\Lead e')
 			->innerJoin('e.account', 'a')
@@ -76,38 +82,39 @@ class LeadController extends AbstractCrudController
 		$paginator->setCurrentPageNumber($page);
 		$paginator->setPageRange($this->paginatorRange);
 		
-		$ui = [
-				'table' => [
-						"referrer" => [
+		$ui = [ 
+				'table' => [ 
+						"referrer" => [ 
 								"col" => 6,
 								"label" => "Source",
-								"sort" => true
+								"sort" => true 
 						],
-						"timecreated" => [
+						"lastsubmitted" => [ 
 								"col" => 4,
-								"label" => "Date",
-								"sort" => true
-						]
-				]
+								"label" => "Submitted",
+								"sort" => true 
+						] 
+				] 
 		];
 		
 		$filters = $this->getFilterForm($this->params()
 			->fromQuery())
 			->remove('account');
 		
-		return [
+		return [ 
 				'id' => $id,
 				'paginator' => $paginator,
 				'sort' => $sort,
 				'order' => $order,
 				'page' => $page,
-				'query' => $this->params()->fromQuery(),
+				'query' => $this->params()
+					->fromQuery(),
 				'filters' => $filters,
-				'ui' => $ui
+				'ui' => $ui 
 		];
 	}
 
-	public function getForm ($entityClass = null)
+	public function getForm($entityClass = null)
 	{
 		$form = parent::getForm($entityClass);
 		
@@ -115,9 +122,11 @@ class LeadController extends AbstractCrudController
 			$entityClass = $entityClass ?  : $this->getEntityClass();
 			$hydrator = new DoctrineHydrator($this->getEntityManager(), $entityClass);
 			$hydrator->addStrategy('timecreated', new DateTimeStrategy());
+			$hydrator->addStrategy('lastsubmitted', new DateTimeStrategy());
 			$form->setHydrator($hydrator);
 			if ($form->has('submit')) {
-				$form->get('submit')->setLabel('Save');
+				$form->get('submit')
+					->setLabel('Save');
 			}
 			if ($form->has('cancelar')) {
 				$form->get('cancelar')
@@ -128,52 +137,54 @@ class LeadController extends AbstractCrudController
 		return $form;
 	}
 
-	protected function getFilterForm ($data = array())
+	protected function getFilterForm($data = array())
 	{
 		$sl = $this->getServiceLocator();
-		$form = $sl->get('FormElementManager')->get('Lead\Form\FilterForm');
+		$form = $sl->get('FormElementManager')
+			->get('Lead\Form\FilterForm');
 		$form->setInputFilter($form->getInputFilter());
 		if ($data) {
 			$form->setData($data);
-			if (! $form->isValid()) {
-				$form->setData(array());
+			if (!$form->isValid()) {
+				$form->setData(array ());
 			}
 		}
 		return $form;
 	}
 
-	public function handleSearch (Builder $qb)
+	public function handleSearch(Builder $qb)
 	{
-		$query = $this->getRequest()->getQuery();
-		$filters = [
+		$query = $this->getRequest()
+			->getQuery();
+		$filters = [ 
 				'daterange',
-				'referrer'
+				'referrer' 
 		];
 		if ($query) {
-			$where = [];
-			$params = [];
-			foreach ($filters as $condition) {
-				if (isset($query[$condition]) && "" !== $query[$condition]) {
+			$where = [ ];
+			$params = [ ];
+			foreach ( $filters as $condition ) {
+				if (isset($query [$condition]) && "" !== $query [$condition]) {
 					switch ($condition) {
-						case 'daterange':
-							list ($from, $to) = array_map(function  ($d)
-							{
+						case 'daterange' :
+							list ( $from, $to ) = array_map(function ($d) {
 								return date('Y-m-d', strtotime($d));
-							}, explode("-", $query[$condition]));
-							$where['from'] = $from;
-							$where['to'] = $to;
+							}, explode("-", $query [$condition]));
+							$where ['from'] = $from;
+							$where ['to'] = $to;
 							$qb->andWhere($qb->expr()
-								->between("e.timecreated", ":from", ":to"));
+								->between("e.lastsubmitted", ":from", ":to"));
+							// ->between( "e.timecreated", ":from", ":to" ) );
 							break;
-						case 'referrer':
-							$where['referrer'] = "%://{$query[$condition]}%";
+						case 'referrer' :
+							$where ['referrer'] = "%://{$query[$condition]}%";
 							$qb->andWhere("e.referrer LIKE :referrer");
 							break;
 					}
 				}
 			}
 			if ($where) {
-				foreach ($where as $key => $value) {
+				foreach ( $where as $key => $value ) {
 					$qb->setParameter($key, $value);
 				}
 			}
