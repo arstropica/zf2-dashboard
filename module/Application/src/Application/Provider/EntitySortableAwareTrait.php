7a6f05180f3e587581eb3cc863628f00c9656362
ArsTropica <aowilliams@arstropica.com>
2016-02-05 18:20:32 -0500

    Stripped assoc keys from entity array.

--- module/Application/src/Application/Provider/EntitySortableAwareTrait.php
+++ module/Application/src/Application/Provider/EntitySortableAwareTrait.php
@@ -1,5 +1,7 @@
 <?php
+
 namespace Application\Provider;
+
 use Doctrine\ORM\EntityManager;
 
 /**
@@ -7,13 +9,13 @@ use Doctrine\ORM\EntityManager;
  * @author arstropica
  *        
  */
-trait EntitySortableAwareTrait
-{
+trait EntitySortableAwareTrait {
 
-	function fetchByOrder ($sort, $order = 'asc', $reindex = true, $fixed_id = null)
+	function fetchByOrder($sort, $order = 'asc', $reindex = true, $fixed_id = null)
 	{
 		/* @var $qb \Doctrine\ORM\QueryBuilder */
-		$qb = $this->getEntityManager()->createQueryBuilder();
+		$qb = $this->getEntityManager()
+			->createQueryBuilder();
 		$qb->add('select', 'e')
 			->add('from', $this->getEntityClass() . ' e')
 			->addSelect('-e.' . $sort . ' AS HIDDEN sort')
@@ -28,60 +30,61 @@ trait EntitySortableAwareTrait
 		return $results;
 	}
 
-	protected function _reverse ($order)
+	protected function _reverse($order)
 	{
 		return strtolower($order) == 'asc' ? 'desc' : 'asc';
 	}
 
-	protected function _move (&$array, $from, $to)
+	protected function _move(&$array, $from, $to)
 	{
-		if (! isset($array[$from]) || $from == $to) {
+		if (!isset($array [$from]) || $from == $to) {
 			return;
 		}
 		$out = array_splice($array, $from, 1);
 		array_splice($array, $to, 0, $out);
 	}
 
-	protected function _reindex ($array, $sort, $fixed_id = null)
+	protected function _reindex($array, $sort, $fixed_id = null)
 	{
-		$reindexed = [];
+		$reindexed = [ ];
 		$new_index = 0;
 		$fixed_index = null;
 		if ($array) {
-			for ($i = 0; $i < count($array); $i ++) {
-				$result = $array[$i];
-				if (isset($fixed_id) && $fixed_id == $result['id']) {
-					$fixed_index = $result[$sort];
+			$array = array_values($array);
+			for($i = 0; $i < count($array); $i++) {
+				$result = $array [$i];
+				if (isset($fixed_id) && $fixed_id == $result ['id']) {
+					$fixed_index = $result [$sort];
 					if ($fixed_index === $new_index) {
-						$reindexed[$new_index] = $result;
-					} elseif (isset($reindexed[$fixed_index])) {
-						$reindexed[$new_index] = $reindexed[$fixed_index];
-						$reindexed[$fixed_index] = $result;
+						$reindexed [$new_index] = $result;
+					} elseif (isset($reindexed [$fixed_index])) {
+						$reindexed [$new_index] = $reindexed [$fixed_index];
+						$reindexed [$fixed_index] = $result;
 					} else {
-						$reindexed[$fixed_index] = $result;
+						$reindexed [$fixed_index] = $result;
 					}
-				} elseif (isset($reindexed[$new_index])) {
-					$new_index ++;
-					$result[$sort] = $new_index;
-					$reindexed[$new_index] = $result;
+				} elseif (isset($reindexed [$new_index])) {
+					$new_index++;
+					$result [$sort] = $new_index;
+					$reindexed [$new_index] = $result;
 				} else {
-					$result[$sort] = $new_index;
-					$reindexed[$new_index] = $result;
+					$result [$sort] = $new_index;
+					$reindexed [$new_index] = $result;
 				}
-				$new_index ++;
+				$new_index++;
 			}
 			$array = $reindexed;
 		}
 		return $array;
 	}
 
-	public abstract function getEntityClass ();
+	public abstract function getEntityClass();
 
-	public abstract function setEntityClass ($entityClass);
+	public abstract function setEntityClass($entityClass);
 
-	public abstract function setEntityManager (EntityManager $em);
+	public abstract function setEntityManager(EntityManager $em);
 
-	public abstract function getEntityManager ();
+	public abstract function getEntityManager();
 }
 
 ?>
