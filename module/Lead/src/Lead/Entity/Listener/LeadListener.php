59838f6036a93cf185cebe96b18fbbe9b5999294
ArsTropica <aowilliams@arstropica.com>
2016-02-06 16:02:18 -0500

    Assign ipv4 address at Post Load.

--- module/Lead/src/Lead/Entity/Listener/LeadListener.php
+++ module/Lead/src/Lead/Entity/Listener/LeadListener.php
@@ -1,24 +1,26 @@
 <?php
+
 namespace Lead\Entity\Listener;
+
 use Doctrine\Common\Collections\ArrayCollection;
 use Lead\Entity\Lead;
 use Doctrine\ORM\Event\LifecycleEventArgs;
 use Doctrine\ORM\Mapping as ORM;
+use Application\Utility\Helper;
 
 /**
  *
  * @author arstropica
  *        
  */
-class LeadListener
-{
-
+class LeadListener {
+	
 	private $id;
 
 	/**
 	 * Constructor
 	 */
-	public function __construct ()
+	public function __construct()
 	{}
 
 	/**
@@ -27,29 +29,32 @@ class LeadListener
 	 * @param Lead $lead        	
 	 * @param LifecycleEventArgs $event        	
 	 */
-	public function postLoad (Lead $lead, LifecycleEventArgs $event)
+	public function postLoad(Lead $lead, LifecycleEventArgs $event)
 	{
 		// Get the values for the ArrayCollection and sort it using the function
 		$attributes = $lead->getAttributes();
 		
 		// sort as you like
-		usort($attributes, 
-				function  ($a, $b)
-				{
-					$aOrder = $bOrder = 0;
-					$aAttribute = $a->getAttribute();
-					if ($aAttribute) {
-						$aOrder = $aAttribute->getAttributeOrder();
-					}
-					$bAttribute = $b->getAttribute();
-					if ($bAttribute) {
-						$bOrder = $bAttribute->getAttributeOrder();
-					}
-					return $aOrder - $bOrder;
-				});
+		usort($attributes, function ($a, $b) {
+			$aOrder = $bOrder = 0;
+			$aAttribute = $a->getAttribute();
+			if ($aAttribute) {
+				$aOrder = $aAttribute->getAttributeOrder();
+			}
+			$bAttribute = $b->getAttribute();
+			if ($bAttribute) {
+				$bOrder = $bAttribute->getAttributeOrder();
+			}
+			return $aOrder - $bOrder;
+		});
 		
 		// Clear the current collection values and reintroduce in new order.
 		$lead->setAttributes(new ArrayCollection($attributes));
+		
+		$default_ip = "0.0.0.0";
+		$ip = $lead->getIpaddress();
+		$ip_filtered = Helper::validate_ipv4($ip, false) ? $ip : $default_ip;
+		$lead->setIpv4address($ip_filtered);
 	}
 }
 
