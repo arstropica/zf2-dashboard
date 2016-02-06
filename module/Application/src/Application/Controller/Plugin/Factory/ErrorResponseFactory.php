b07bb341c3cf6da8d7d53e47be6c5e254c09922b
ArsTropica <aowilliams@arstropica.com>
2016-02-05 19:17:58 -0500

    Added service factories for HTTP Response handler plugins.

---
+++ module/Application/src/Application/Controller/Plugin/Factory/ErrorResponseFactory.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Application\Controller\Plugin\Factory;
+
+use Zend\ServiceManager\FactoryInterface;
+use Zend\ServiceManager\ServiceLocatorInterface;
+use Application\Controller\Plugin\ErrorResponse;
+
+/**
+ *
+ * @author arstropica
+ *        
+ */
+class ErrorResponseFactory implements FactoryInterface {
+
+	public function createService(ServiceLocatorInterface $pluginManager)
+	{
+		$serviceManager = $pluginManager->getServiceLocator();
+		
+		return new ErrorResponse($serviceManager);
+	}
+}
---
+++ module/Application/src/Application/Controller/Plugin/Factory/JsonErrorResponseFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Application\Controller\Plugin\Factory;
+
+use Zend\ServiceManager\FactoryInterface;
+use Zend\ServiceManager\ServiceLocatorInterface;
+use Application\Controller\Plugin\JSONErrorResponse;
+
+/**
+ *
+ * @author arstropica
+ *        
+ */
+class JsonErrorResponseFactory implements FactoryInterface {
+
+	public function createService(ServiceLocatorInterface $pluginManager)
+	{
+		$serviceManager = $pluginManager->getServiceLocator();
+		
+		return new JSONErrorResponse($serviceManager);
+		// return
+		// $serviceManager->get('Application\Controller\Plugin\JSONErrorResponse');
+	}
+}
