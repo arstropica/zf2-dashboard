3db666ec6c015b5777d806db14c24c35be314a6d
ArsTropica <aowilliams@arstropica.com>
2016-02-22 13:23:29 -0500

    Added active condition to duplicate check.

--- module/Lead/src/Lead/Controller/ImportController.php
+++ module/Lead/src/Lead/Controller/ImportController.php
@@ -1020,6 +1020,15 @@ JTPL;
 				}
 			}
 		}
+		
+		if ($where) {
+			$where = [ 
+					'and' => [ 
+							'active' => 1 
+					] 
+			];
+		}
+		
 		// Execute Query
 		if (isset($where ['and'])) {
 			/* @var $lead \Lead\Entity\Lead */
